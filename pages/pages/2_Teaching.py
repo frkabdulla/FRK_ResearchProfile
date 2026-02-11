c5e1d993b82d612a1919bcf08019f3de7425a3be
Faruk Abdulla <farukabdulla237@gmail.com>
2026-02-11 22:49:11 +0530

    Add Teaching and Academic Roles page

---
+++ pages/pages/2_Teaching.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("🎓 Teaching & Academic Roles")
+
+st.markdown("""
+### Current Roles
+- Assistant Professor (IT & CS)
+- Industry Embedded Program Coordinator
+- Project Coordinator
+
+### Education
+- MCA – Marwadi University (2021–2023)
+- BSc – RDS College (2016–2019)
+""")
