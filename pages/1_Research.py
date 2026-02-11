85a1ba576b814c1ceebe883dc0eae6bb406eeae1
Faruk Abdulla <farukabdulla237@gmail.com>
2026-02-11 22:48:44 +0530

    Add research publications section to 1_Research.py

---
+++ pages/1_Research.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("📚 Research & Publications")
+
+st.markdown("""
+### 2025 Publications
+
+- Machine Learning Techniques for Identifying DDoS Attacks in Cloud Computing  
+- Machine Learning Models for Agro-Climatic Decision Support Systems  
+- Advancing Smart & Sustainable Construction  
+- Innovative Sustainable Materials & Circular Economy  
+""")
+
+st.markdown("---")
+
+st.write("Peer Reviewer: Franklin Open, IGI Global")
