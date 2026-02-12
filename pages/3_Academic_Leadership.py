f130b588dd1acf216f64d1a640c414ec5773024e
Faruk Abdulla <farukabdulla237@gmail.com>
2026-02-12 12:55:33 +0530

    Add Academic Leadership and Peer Review information

---
+++ pages/3_Academic_Leadership.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("🎓 Academic Leadership & Peer Review")
+
+st.markdown("""
+### Current Roles
+- Assistant Professor (IT & CS), Parul University
+- Industry Embedded Program Coordinator
+- Project Coordinator
+
+### Peer Review Contributions
+IGI Global Scientific Publishing (Hershey, US)
+
+Review Identifiers:
+- 9d3ea3e4-1b53-4cc5-baf8-82ddad2e270e
+- a84bf570-93c1-4d2e-9c6f-08e1f9ad217d
+- 5cfc0836-0ac3-4e1b-bcf8-c9541f9e4516
+""")
